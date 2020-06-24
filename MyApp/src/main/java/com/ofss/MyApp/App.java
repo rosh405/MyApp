dc3101d2876700f2f5bd825d9672f2f642da4e49
Rosh405 <roshnroy.90@gmail.com>
2020-06-24 11:24:26 +0530

    my commit

---
+++ MyApp/src/main/java/com/ofss/MyApp/App.java
@@ -0,0 +1,15 @@
+package com.ofss.MyApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Calculator c1 = new Calculator();
+        System.out.println(c1.add(1, 2));
+    	System.out.println( "Hello World!" );
+    }
+}
